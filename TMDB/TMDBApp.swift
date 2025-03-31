aeb3f4520e318fb033539c1482cc02c0378c2f7a
Ivan Lopez Ansaldo <ivan.loa.33@gmail.com>
2025-03-31 07:06:27 -0600

    Initial Commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Ivan Lopez Ansaldo on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
